024bd40e360154f440e2a73fd054adebab327d18
Petr Zhalybin <pjalybin@gmail.com>
2026-01-01 18:39:04 +0100

    screenshot shorcuts

--- config.def.h
+++ config.def.h
@@ -94,6 +94,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,			XK_F8,     spawn,       SHCMD("screenshot") },
+	{ MODKEY|ShiftMask,		XK_F8,     spawn,       SHCMD("screenshot color") },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
